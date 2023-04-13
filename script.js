24fca1561b6bfd605e77003b2ee8db5d72d2e4ff
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-04-13 22:43:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function decimalToBinary(decimal) {
+  let binary = "";
+  while (decimal > 0) {
+    let remainder = decimal % 2;
+    binary = remainder + binary;
+    decimal = Math.floor(decimal / 2);
+  }
+  return binary;
 }
-
-module.exports = threeSum;
